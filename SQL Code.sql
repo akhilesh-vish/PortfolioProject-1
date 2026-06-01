0841d647489acfcf7dbce244434ed405f7bc9d1d
Akhilesh Vishwakarma <akhileshvidisha@gmail.com>
2026-06-01 20:13:46 +0530

    Update SQL Code.sql

--- SQL Code.sql
+++ SQL Code.sql
@@ -132,7 +132,7 @@ Queries used for Tableau Project
 -- 1. 
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-From PortfolioProject..CovidDeaths
+From CovidDeaths
 where continent is not null 
 order by 1,2
 
@@ -140,7 +140,7 @@ order by 1,2
 -- 2. 
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
-From PortfolioProject..CovidDeaths
+From CovidDeaths
 Where continent is null 
 and location not in ('World', 'European Union', 'International')
 Group by location
@@ -150,7 +150,7 @@ order by TotalDeathCount desc
 -- 3.
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
+From CovidDeaths
 Group by Location, Population
 order by PercentPopulationInfected desc
 
@@ -159,7 +159,7 @@ order by PercentPopulationInfected desc
 
 
 Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths
+From CovidDeaths
 Group by Location, Population, date
 order by PercentPopulationInfected desc
 
